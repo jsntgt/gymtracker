0a229b3e3a20b5a14ee81d566e81221f47adbd2e
Michal Zarnowski <zar.michal@wp.pl>
2021-09-07 17:57:15 +0200

    create register page

--- gymtracker/gymtracker/urls.py
+++ gymtracker/gymtracker/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('product/<int:id>', ProductDetailView.as_view(), name="product_detail"),
     path('login/', auth_view.LoginView.as_view(template_name="users/login.html"), name="login"),
     path('logout/', auth_view.LogoutView.as_view(template_name="users/logout.html"), name="logout"),
+    path('register/', user_views.register, name="register"),
     path('', include('users.urls')),
 
 ]
--- gymtracker/users/views.py
+++ gymtracker/users/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm
 
 
 def home(request):
     return render(request, 'users/home.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(request, 'Your account has been created! You are able to log in')
+            return redirect('home')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
 
