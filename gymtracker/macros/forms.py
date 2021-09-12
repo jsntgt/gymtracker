5ee7ac0e9173d7c4af15158782c0a990431f911d
Krzysztof Witucki <krzysiek.witucki55@gmail.com>
2021-09-12 15:17:58 +0200

    add image, nutritional_values_weight fields to Product model add styles to product_detail, product_form templates add content to product_detail template

--- gymtracker/macros/forms.py
+++ gymtracker/macros/forms.py
@@ -6,3 +6,7 @@ class ProductForm(ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super(ProductForm, self).__init__(*args, **kwargs)
+        self.fields['nutritional_values_weight'].label = "Nutritional values per"
--- gymtracker/macros/models.py
+++ gymtracker/macros/models.py
@@ -5,16 +5,23 @@ UNIT_CHOICES = (
     ("gram", "g"),
     ("milliliter", "ml")
 )
+WEIGHT_VALUES_CHOICES = (
+    ("100g", "100g"),
+    ("100ml", "100ml"),
+    ("serving", "serving")
+)
 
 
 class Product(models.Model):
     name = models.CharField(max_length=50)
     weight = models.IntegerField()
     weight_unit = models.CharField(max_length=10, choices=UNIT_CHOICES, default="gram")
+    nutritional_values_weight = models.CharField(max_length=10, choices=WEIGHT_VALUES_CHOICES, default="100g")
     energy = models.DecimalField(max_digits=10, decimal_places=1)
     fats = models.DecimalField(max_digits=10, decimal_places=1)
     carbohydrates = models.DecimalField(max_digits=10, decimal_places=1)
     proteins = models.DecimalField(max_digits=10, decimal_places=1)
+    image = models.ImageField(default='', upload_to='product_images')
 
     def get_absolute_url(self):
         return reverse("product_detail", kwargs={"id": self.pk})
